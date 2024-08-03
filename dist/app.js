e0fda1b0d78ff2511fc4dc948a5440f2f607f83c
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-08-03 16:34:31 +0530

    remove vercel

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -42,17 +33,14 @@ function configureApp(app) {
     app.use((0, compression_1.default)());
     // routes
     app.use("/api/auth", routes_1.authRoutes);
-    //
     // health check
-    app.get("/", (req, res, next) => __awaiter(this, void 0, void 0, function* () {
-        res.status(200).json({
-            message: "server is running",
-        });
-    }));
+    app.get("/", (req, res) => {
+        res.send("Hello, World!");
+    });
     // 404 error handler
     app.use((req, res, next) => {
         res.status(404).json({
-            message: "Not Found",
+            message: "Not Found - 404",
         });
     });
     // global error handler
